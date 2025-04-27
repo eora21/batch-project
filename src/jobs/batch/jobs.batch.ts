bfd487f7fe8724e36a19a9199c62795e9b088b28
eora21 <eora21@naver.com>
2025-04-27 15:36:37 +0900

    style: 💄 배치 로그에서 '몇 개의 작업이 처리되었는지'만 나타내도록 작성
    실제 pending 작업이 completed 되었는지는 배치 레이어에서 알 수 없으므로, service가 반환하는 개수에 대해서만
    로그를 작성하도록 설정

--- src/jobs/batch/jobs.batch.ts
+++ src/jobs/batch/jobs.batch.ts
@@ -1,6 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { JobsService } from '../service/jobs.service';
-import { JobStatus } from '../model/job.status';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
@@ -14,6 +13,6 @@ export class JobsBatch {
   @Cron(CronExpression.EVERY_MINUTE)
   async pendingToComplete() {
     const updateCount = await this.jobsService.updatePendingJobToCompleteJob();
-    this.logger.log(`총 ${ updateCount }개의 job이 배치 처리됨(${ JobStatus.PENDING } -> ${ JobStatus.COMPLETED })`);
+    this.logger.log(`총 ${ updateCount }개의 job이 배치 처리됨`);
   }
 }
