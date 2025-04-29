b139763b4569c50c12b5db0fd3d48684ae530592
eora21 <eora21@naver.com>
2025-04-29 16:51:03 +0900

    style: 💄 e2e 테스트명, import 경로 변경

--- test/app.e2e.spec.ts
+++ test/app.e2e.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('e2e', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
